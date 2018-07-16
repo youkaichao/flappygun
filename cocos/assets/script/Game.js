546ffe54d1694eeeea22955b095271c3245ca8db
AerysNan <nany16@163.com>
2018-07-16 20:17:18 +0800

    add node pool and end game

--- cocos/assets/script/Game.js
+++ cocos/assets/script/Game.js
@@ -52,17 +52,20 @@ cc.Class({
     gameStart: false,
     maxHeight: 0,
     score: 0,
+    window: 0,
     heightPerWindow: 0,
     deadlineSpacing: 400
   },
   initPhysics: function() {
     var physicsManager = cc.director.getPhysicsManager();
+    this.physicsManager = physicsManager;
     physicsManager.enabled = true;
     physicsManager.debugDrawFlags = cc.PhysicsManager.DrawBits.e_aabbBit |cc.PhysicsManager.DrawBits.e_pairBit |cc.PhysicsManager.DrawBits.e_centerOfMassBit |cc.PhysicsManager.DrawBits.e_jointBit |cc.PhysicsManager.DrawBits.e_shapeBit;
     physicsManager.gravity = cc.v2(0, -this.gravity);
   },
   initCollide: function() {
     var collisionManager = cc.director.getCollisionManager();
+    this.collisionManager = collisionManager;
     collisionManager.enabled = true;
     collisionManager.enabledDebugDraw = true;
     collisionManager.enabledDrawBoundingBox = true;
@@ -83,11 +86,10 @@ cc.Class({
     this.setupScene();
     this.setupInputControl();
     this.initCollide();
-    this.spawnNewObject("coin");
+    this.spawnNewObject();
   },
   setupScene: function() {
     this.title.active = false;
-    console.log(this.player.flameAnimation.node.active);
     this.player.node.active = true;
     this.player.flameAnimation.node.active = true;
     this.deadline.active = true;
@@ -98,6 +100,12 @@ cc.Class({
     this.textUpper[0].active = false;
     this.textUpper[1].active = false;
     this.gameStart = true;
+    this.player.node.setPosition(0, 0);
+    this.maxHeight = 0;
+    this.score = 0;
+    this.window = 0;
+    this.camera.enabled = true;
+    this.spawnCoin(0, this.player.node.height * 2);
   },
   setupInputControl: function() {
     var self = this;
@@ -109,7 +117,7 @@ cc.Class({
           self.player.rigidBody.type = cc.RigidBodyType.Dynamic;
         }
         self.player.fireAnimation.play("pistol_fire");
-        console.log(self.player.flameAnimation.play("spark_flame").duration);
+        self.player.flameAnimation.play("spark_flame");
         self.player.rigidBody.applyLinearImpulse(self.player.rigidBody.getWorldVector(new cc.Vec2(-self.player.recoil, 0)), self.player.rigidBody.getWorldPoint(new cc.Vec2(self.player.recoilPosX, self.player.recoilPosY)), true);
       }
     }, self.node)
@@ -152,51 +160,85 @@ cc.Class({
     this.textUpper[0].setPositionX(pos0U + 5);
     this.textUpper[1].setPositionX(pos1U + 5);
   },
-  spawnNewObject: function(name) {
-    switch(name){
-      case "coin":
-        this.spawnCoin();
-        break;
-      case "bullet":
-        this.spawnBullet();
-        break;
-    }
-  },
-  spawnCoin: function() {
+  spawnCoin: function(posX, posY) {
     var newCoin = null;
-    if(this.coinPool.size() > 0)
+    if(this.coinPool.size() > 0){
       newCoin = this.coinPool.get(this);
+      newCoin.getComponent("Coin").enabled = true;
+    }
     else
       newCoin = cc.instantiate(this.coinPrefab);
     this.node.addChild(newCoin);
+    newCoin.setPosition(posX, posY);
     newCoin.getComponent("Coin").game = this;
     this.camera.addTarget(newCoin);
   },
-  spawnBullet: function() {
+  spawnBullet: function(posX, posY) {
     var newBullet = null;
-    if(this.bulletPool.size() > 0)
+    if(this.bulletPool.size() > 0){
       newBullet = this.BulletPool.get(this);
+      newBullet.getComponent("Bullet").enabled = true;
+    }
     else
       newBullet = cc.instantiate(this.bulletPrefab);
     this.node.addChild(newBullet);
+    this.node.setPosition(posX, posY);
     newBullet.getComponent("Bullet").game = this;
     this.camera.addTarget(newBullet);
   },
   heightUpdate: function() {
     this.maxHeight = Math.max(this.player.node.y, this.maxHeight);
     this.deadline.setPosition(this.player.node.x, this.maxHeight - this.deadlineSpacing);
-    if(this.player.node.y * this.heightPerWindow / cc.winSize.height > this.score){
+    if(Math.round(this.player.node.y * this.heightPerWindow / cc.winSize.height) > this.score){
       this.score = Math.round(this.player.node.y * this.heightPerWindow / cc.winSize.height);
       this.scoreLabel.string = "Current Score: " + this.score;
     }
   },
+  spawnNewObject: function() {
+//    var mode = Math.floor(4 * cc.random0To1());
+    var mode = 0;
+    switch(mode) {
+      case 0:
+        for(var i = 0; i < 5; i++){
+          for(var j = -1; j < 2; j++){
+            this.spawnCoin(this.background[0].x + j * cc.winSize.width / 4, (this.window + 1) * cc.winSize.height + (i - 2) * cc.winSize.height / 7);
+            this.spawnCoin(this.background[1].x + j * cc.winSize.width / 4, (this.window + 1) * cc.winSize.height + (i - 2) * cc.winSize.height / 7);
+          }
+        }
+        break;
+      case 1:
+        
+        break;
+      case 2: 
+        break;
+      case 3:
+        break;
+    }
+  },
+  objectUpdate: function() {
+    if(Math.round(this.player.node.y / cc.winSize.height) > this.window) {
+      this.window = Math.round(this.player.node.y / cc.winSize.height);
+      if(this.window % 2 == 0)
+        this.spawnNewObject();   
+    }
+  },
   endGame: function() {
-
+    this.physicsManager.enabled = false;
+    this.collisionManager.enabled = false;
+    this.player.node.active = false;
+    this.deadline.active = false;
+    var coinList = this.node.getComponentsInChildren("Coin");
+    for(var i = 0; i < coinList.length; i++)
+      this.despawnCoin(coinList[i].node);
+    var bulletList = this.node.getComponentsInChildren("Bullet");
+//    for(var i = 0; i )
   },
   despawnCoin: function(node) {
+    this.camera.removeTarget(node);
     this.coinPool.put(node);
   },
   despawnBullet: function(node) {
+    this.camera.removeTarget(node);
     this.bulletPool.put(node);
   },
   update: function(dt) {
@@ -205,6 +247,7 @@ cc.Class({
     if(this.gameStart){
       this.backgroundShift();
       this.heightUpdate();
+      this.objectUpdate();
       if(this.deadline.y > this.player.node.y + Math.abs(this.player.node.width / 2 * Math.sin(this.player.node.rotation / 180 * Math.PI))){
         this.endGame();
       }
